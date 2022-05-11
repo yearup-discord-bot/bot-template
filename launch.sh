89ea8eea513c41b3808dfe9fd7bd787c4a6f0cc3
carloscruzyearup <carlos.cruz3@seattlecolleges.edu>
2022-05-11 10:34:40 -0700

    fix launch.sh infinite loop bug
    validate that git succesfully checked out an earlier commit, if not exit
    the program with a 'FATAL' warning; also i color coded the messages
    according to severity warning: yellow fatal: red

--- launch.sh
+++ launch.sh
@@ -11,11 +11,22 @@ function main()
 	node index.js >> "../logs/$(date +%m-%d-%y).${2}.log" 2>&1
 	
 	if [ "$?" != 0 ]; then
+
 		let "COMMITSBEHIND+=1"
+
+		YELLOW='\033[0;33m'
 		RED='\033[0;31m'
 		NC='\033[0m'
-		echo -e "${RED}!!-- WARNING CURRENTLY RUNNING @ $COMMITSBEHIND COMMIT(S) BEHIND TIP --!!${NC}"
+
 		git checkout HEAD~1
+
+		if [ "$?" != 0 ]; then 
+			echo -e "${RED}!!-- FATAL COULD NOT FIND A STABLE COMMIT! EXITING --!!${NC}"
+			exit
+		fi
+
+
+		echo -e "${YELLOW}!!-- WARNING CURRENTLY RUNNING $COMMITSBEHIND COMMIT(S) BEHIND TIP --!!${NC}"
 
 		main
 	fi
@@ -29,7 +40,7 @@ elif [ "$1" = "debug" ]; then
 	main COMMITSBEHIND $1
 else
 	echo -e "Please specify whether this is a 'release' or 'debug' launch.\nex. './launch [release or debug]'"
-	exit;
+	exit
 fi
 
 main COMMITSBEHIND
